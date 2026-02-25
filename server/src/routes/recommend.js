626bdd44bb55a0a6515e5e5693488200587944d7
Gurmehr Singh Gulati <93367350+gurmehr04@users.noreply.github.com>
2026-02-25 23:16:32 +0530

    Delete server directory

--- server/src/routes/recommend.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getRecommendations } = require('../controllers/recommendController');
-
-router.post('/', protect, getRecommendations);
-
-module.exports = router;
